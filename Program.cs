188169d7831b463c20fb180e6f3a3dbe4770d8a0
Will <doggy.huang@gmail.com>
2021-07-04 16:26:25 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstAntlr4Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
